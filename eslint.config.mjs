cf6c39b18b1d33771dd7b17283d1f7f572ed97a6
Rick Allen <rick@Ricks-MacBook-Pro-16.local>
2025-04-29 01:19:56 -0700

    Update ESLint configuration and package scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import jestA11y from 'eslint-plugin-jsx-a11y';
+import nextPlugin from 'eslint-config-next';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,8 +11,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+export default [
+  ...compat.extends('next/core-web-vitals'),
+  nextPlugin(),
   jestA11y.configs.recommended,
   {
     rules: {
@@ -26,5 +28,3 @@ const eslintConfig = [
     }
   }
 ];
-
-export default eslintConfig;
