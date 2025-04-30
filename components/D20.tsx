e3eb50f88e5a6494ed0a16a012f63d62d5a895f9
Rick Allen <rick@Ricks-MacBook-Pro-16.local>
2025-04-30 10:24:21 -0700

    tweak D20 opacity and spin

--- components/D20.tsx
+++ components/D20.tsx
@@ -2,11 +2,11 @@
 
 import React, { useRef } from 'react';
 import { useFrame, ThreeElements } from '@react-three/fiber';
-import { Mesh, WireframeGeometry, LineSegments, LineBasicMaterial, IcosahedronGeometry, Group } from 'three';
+import { WireframeGeometry, LineSegments, LineBasicMaterial, IcosahedronGeometry, Group } from 'three';
 
 function createD20Wireframe(radius: number = 0.5, scale: number = 1): LineSegments {
   const geometry = new WireframeGeometry(new IcosahedronGeometry(radius, 0));
-  const material = new LineBasicMaterial({ color: 0x02816F, opacity: 1, transparent: true });
+  const material = new LineBasicMaterial({ color: 0x02816F, opacity: 0.5, transparent: true });
   const wireframe = new LineSegments(geometry, material);
   wireframe.scale.set(scale, scale, scale);
   return wireframe;
@@ -24,8 +24,8 @@ export default function ShadowForgeLogo3D(props: ThreeElements['group']) {
 
       // Die rotation
       if (wireD20Ref.current) {
-        wireD20Ref.current.rotation.x = Math.sin(time) * 0.2;
-        wireD20Ref.current.rotation.z = Math.cos(time) * 0.2;
+        wireD20Ref.current.rotation.x = Math.sin(time) * 1;
+        wireD20Ref.current.rotation.z = Math.cos(time) * 1;
       }
     }
   });
