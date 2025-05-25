aec88791f42efff97b577762fb340410cbc1a3d9
Rick Allen <rick@Ricks-MacBook-Air.local>
2025-05-25 10:31:27 -0700

    clarify unicode sanitation

--- lib/conversion/sanitizeText.ts
+++ lib/conversion/sanitizeText.ts
@@ -1,7 +1,10 @@
 export function sanitizeText(text: string): string {
   return text
+    // replace invalid characters from OCR with [INVALID] that LLM will ignore
     .replace(/\uFFFD/g, '[INVALID]')
+    // remove control characters
     .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g, '')
+    // remove zero width spaces and other control characters
     .replace(/[\u200B-\u200F\u2028-\u202F\u00AD]/g, '')
     // convert \r\n and \r to \n
     .replace(/\r\n|\r/g, '\n')
